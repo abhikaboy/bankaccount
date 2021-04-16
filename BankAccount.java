baf498a9753c677a60645946df302546850eb8cb
Suntex Ray <abhikaboy@gmail.com>
2021-04-16 10:06:23 -0400

    eeeee

--- BankAccount.java
+++ BankAccount.java
@@ -8,9 +8,7 @@ public class BankAccount {
     private static String routingNumber = "";
     private static double totalBalance = 0;
 
-    public BankAccount(int accountNum, double accountBalance, 
-    String password, String customerName)
-    {
+    public BankAccount(int accountNum, double accountBalance, String password, String customerName) {
         this.accountNum = accountNum;
         this.accountBalance = accountBalance;
         this.password = password;
@@ -23,7 +21,19 @@ public class BankAccount {
                 + bankAddress;
     }
 
-    public BankAccount(int n, String p, String c){
+    public void withdraw(double n) {
+        if (n < 0) {
+            System.out.println("Can't withdraw a negative amount");
+        }
+
+        else if (accountBalance - n >= 0) {
+            accountBalance -= n;
+        } else {
+            System.out.println("Withdraw rejected - withdrawal amount would put the account into a negative balance");
+        }
+    }
+
+    public BankAccount(int n, String p, String c) {
         accountNum = n;
         password = p;
         customerName = c;
--- BankAccountTest.java
+++ BankAccountTest.java
@@ -1,5 +1,18 @@
 public class BankAccountTest {
     public static void main(String[] args) {
-        BankAccount HammadAccount = new BankAccount(001, );
+        BankAccount.setBankName();
+        BankAccount.setBankAddress();
+        BankAccount.setRoutingNumber();
+        BankAccount[] accounts = { new BankAccount(0, 100, "password1", "bob"),
+                new BankAccount(1, 100, "password2", "jamal"), new BankAccount(2, 100, "password3", "Hammad"),
+                new BankAccount(3, 100, "password4", "Vinu"), new BankAccount(4, 100, "password5", "Abhik"), };
+
+        int widthdrawAmount = 10;
+        int depositAmount = 10;
+
+        accounts[0].withdraw(widthdrawAmount);
+        accounts[1].deposit(depositAmount);
+        accounts[2].changePassword("bababab");
+
     }
 }
