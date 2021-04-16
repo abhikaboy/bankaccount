b48b5b92f98206125060b8c52e7727452ee2ccc0
Suntex Ray <abhikaboy@gmail.com>
2021-04-16 09:25:37 -0400

    init

---
+++ BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount {
+    private int accountNum;
+    private int accountBalance;
+    private String password;
+    private String customerName;
+    private static String bankName;
+    private static String bankAddress;
+    private static String routingNumber;
+    private static int totalBalance;
+
+    public BankAccount
+    {
+
+    }
+}
