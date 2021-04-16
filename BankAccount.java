e22dd8c20513938a8ed9a591e365fc87527fe979
unknown <hammadfarooqi02@gmail.com>
2021-04-16 09:35:38 -0400

    first constructor

--- BankAccount.java
+++ BankAccount.java
@@ -8,8 +8,16 @@ public class BankAccount {
     private static String routingNumber;
     private static int totalBalance;
 
-    public BankAccount
+    public BankAccount(int accountNum, int accountBalance, 
+    String password, String customerName, String bName, String bAddress, String rNum, int totalB)
     {
-
+        this.accountNum = accountNum;
+        this.accountBalance = accountBalance;
+        this.password = password;
+        this.customerName = customerName;
+        bankName = bName;
+        bankAddress = bAddress;
+        routingNumber = rNum;
+        totalBalance = totalB;
     }
 }
